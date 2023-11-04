6f654d0378e47d2a92381c104889a8fd4465036b
MaxNguyen5040 <MaxNguyen5040>
2023-11-04 16:36:32 -0400

    first commit

---
+++ mysite/polls/models.py
@@ -0,0 +1,27 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        # String representation of Question (for development)
+        return self.question_text
+    
+    def was_published_recently(self):
+        # Custom model for seeing how recently a question was published
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        # String representation of Choice (for development)
+        return self.choice_text
